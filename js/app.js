979221939302e2adf57b6bfc90f122a460667f36
Василий <vasiliy@MacBook-Pro-Vasilij.local>
2026-06-30 00:09:09 +0700

    feat: Configure Spring MVC for static resources

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const HOST = '';
+
+$(function () {
+    const navToggle = document.querySelector(".nav-toggle");
+    const navLinks = document.querySelector(".nav-links");
+
+    navToggle.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+    });
+});
