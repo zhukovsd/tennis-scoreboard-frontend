17f3fa7a788d7ade6369de735ac54c8c99c19fc9
Василий <vasiliy@MacBook-Pro-Vasilij.local>
2026-06-30 00:13:04 +0700

    feat: Add match score page

--- js/app.js
+++ js/app.js
@@ -32,6 +32,80 @@ function initNewMatchPage() {
     });
 }
 
+function initMatchScorePage() {
+    var matchUuid;
+
+    $(function () {
+        matchUuid = new URLSearchParams(window.location.search).get('uuid');
+        if (!matchUuid) {
+            $('#error-message').text('No match ID provided.').show();
+            return;
+        }
+
+        loadMatch();
+
+        $(document).on('click', '.point-btn', function () {
+            var playerName = $(this).data('player-name');
+            $.ajax({
+                method: 'POST',
+                url: HOST + 'matches/' + matchUuid + '/point',
+                contentType: 'application/json',
+                data: JSON.stringify({name: playerName}),
+                success: function (match) {
+                    renderMatch(match);
+                },
+                error: function (xhr) {
+                    var message = xhr.responseJSON
+                        ? xhr.responseJSON.message
+                        : 'An error occurred.';
+                    $('#error-message').text(message).show();
+                }
+            });
+        });
+    });
+
+    function loadMatch() {
+        $.get(HOST + 'matches/' + matchUuid, function (match) {
+            renderMatch(match);
+        }).fail(function (xhr) {
+            var message = xhr.responseJSON
+                ? xhr.responseJSON.message
+                : 'Failed to load match.';
+            $('#error-message').text(message).show();
+        });
+    }
+
+    function renderMatch(match) {
+        renderPlayer('#player1', match.firstPlayer);
+        renderPlayer('#player2', match.secondPlayer);
+
+        $('.point-btn').first().data('player-name', match.firstPlayer.name);
+        $('.point-btn').last().data('player-name', match.secondPlayer.name);
+
+        if (match.winnerName) {
+            $('.point-btn').hide();
+            $('#winner-message').show();
+            $('#winner-text').text('Winner: ' + match.winnerName);
+        } else {
+            $('.point-btn').show();
+            $('#winner-message').hide();
+        }
+
+        $('#error-message').hide();
+    }
+
+    function renderPlayer(prefix, player) {
+        $(prefix + '-name').text(player.name);
+        $(prefix + '-sets').text(player.sets);
+        $(prefix + '-games').text(player.games);
+        if (player.tieBreakPoints !== null) {
+            $(prefix + '-points').text(player.tieBreakPoints);
+        } else {
+            $(prefix + '-points').text(player.points);
+        }
+    }
+}
+
 $(function () {
     const navToggle = document.querySelector(".nav-toggle");
     const navLinks = document.querySelector(".nav-links");
@@ -42,5 +116,8 @@ $(function () {
 
     if ($('#new-match-form').length) {
         initNewMatchPage();
+    }
+    if ($('#score-body').length) {
+        initMatchScorePage();
     }
 });
